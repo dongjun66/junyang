577d675ac74933153edfbea8a60dc91d299e7367
lyy <lyy@163.com>
2018-05-22 15:56:32 +0800

    router

--- junyang/src/App.js
+++ junyang/src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css';
-import {}
+import {Route , Switch ,Redirect ,withRouter} from "react-router-dom"
 import Footer from './components/commons/Appfooter'
 import Home from './components/pages/Home'
 import Shops from './components/pages/Shops'
 import Fav from './components/pages/Fav'
 import Email from './components/pages/Email'
 import Mine from './components/pages/Mine'
+import {connect} from "react-redux"
 class App extends Component {
+	 renderRoutes () {//渲染Route路由的方法
+    let { routes } = this.props
+    return routes.map(item => {
+            return (<Route 
+              exact = {item.isExact} 
+              path = {item.path} 
+              key = {item.id}
+              component={item.component} />)
+          })
+  }
   render() {
     return (
-      <div className="App">
-       1111111
+      <div className="app">
         
+        <Switch>    
+          {this.renderRoutes()}
+          <Redirect from = "**" to = {{pathname: '/'}} />
+        </Switch>
+
         <Footer />
       </div>
-    );
+    )
   }
+  
+  
 }
 
-export default App;
+App.defaultProps = {
+  routes: [
+    {id: 1, path: '/', component: Home, isExact: true},
+    {id: 2, path: '/shops', component: Shops},
+    {id: 3, path: '/fav', component: Fav},
+    {id: 4, path: '/email', component: Email},
+    {id: 5, path: '/mine', component: Mine}
+  ]
+}
+export default withRouter(App);
