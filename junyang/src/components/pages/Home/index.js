0c761cc4c2477f7ff4cc4c9b570ec1be702e6e85
dongjun <16600275166@163.com>
2018-05-23 15:22:32 +0800

    改了

--- junyang/src/components/pages/Home/index.js
+++ junyang/src/components/pages/Home/index.js
@@ -6,8 +6,8 @@ import React,{Component} from "react"
  		return(
  			<div className = "home">
  				<Header/>
- 				<Nav/>
- 				<Case/>
+ 				<Section/>
+ 				<Footer/>
  			</div>
  		)
  	}
@@ -17,13 +17,15 @@ import React,{Component} from "react"
  class Header extends Component{
  	render(){
  		return(
- 			<header>
+ 			<div className="head">
+ 				<header>
  					<div><i className="fa fa-plus"></i></div>
  					<div>
  						<i className="fa fa-search"></i><i>搜索菜谱 食材</i><i className="fa fa-microphone"></i>
  					</div>
  					<div><i className="fa fa-shopping-bag"></i></div>
  				</header>
+ 					</div>
  		)
  	}
  }
@@ -33,46 +35,72 @@ import React,{Component} from "react"
  		return(
  			<nav>
  					<div>
-   						<img src={require("../../../assets/images/1.gif")} />
+   						<img src={require("../../../assets/images/1.gif")} alt=""/>
  						<span>厨房好物</span>
  					</div>
  					<div>
-   						<img src={require("../../../assets/images/2.gif")} />
+   						<img src={require("../../../assets/images/2.gif")} alt=""/>
  						<span>厨房好物</span>
  					</div>
  					<div>
-   						<img src={require("../../../assets/images/3.gif")} />
+   						<img src={require("../../../assets/images/3.gif")} alt=""/>
  						<span>厨房好物</span>
  					</div>
  					<div>
-   						<img src={require("../../../assets/images/4.gif")} />
+   						<img src={require("../../../assets/images/4.gif")} alt=""/>
  						<span>厨房好物</span>
  					</div>
  				</nav>
  		)
  	}
  }
- //Case
- class Case extends Component{
- 	render(){
- 		return(
- 			<div className="case">
-   				<img src={require("../../../assets/images/case1.gif")}/>
-   				<img src={require("../../../assets/images/case2.gif")}/>
- 			</div>
- 		)
- 	}
- }
- //section
+ 
+  //section
  class Section extends Component{
  	render(){
  		return(
  			<section>
- 				<div></div>
+ 				<div>
+ 					<Nav/>
+ 					<Case/>
+ 					<Banner/>
+ 				</div>
  			</section>
  		)
  	}
  }
  
  
+ //Case
+ class Case extends Component{
+ 	render(){
+ 		return(
+ 			<div className="case">
+   				<img src={require("../../../assets/images/case1.gif")} alt=""/>
+   				<img src={require("../../../assets/images/case2.gif")} alt=""/>
+ 			</div>
+ 		)
+ 	}
+ }
+
+//banner
+class Banner extends Component{
+	render(){
+
+		return(
+			<div></div>
+			
+		)
+
+			}
+	}
+ 
+ //footer
+ class Footer extends Component{
+ 	render(){
+ 		return(
+ 			<div className="foot"></div>
+ 		)
+ 	}
+ }
  export default Home
